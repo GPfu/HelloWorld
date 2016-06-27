b6e55a22ee4bcc28d6100ad7453b8e0688b1f365
GPfu <instantreplays@comcast.net>
2016-06-27 16:09:07 -0700

    table

--- hello.c
+++ hello.c
@@ -1,2 +1,8 @@
 #include <stdio.h>
-int main() {printf("Hello, world!\n"); return 0; }
+int main() {
+	int x = 0;
+	short y = 52;
+	printf("Var \t Val \n x \t %d \n y \t %d \n", x, y);
+	return 0;
+}
+
