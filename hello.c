fcccc62634a6b68f153f380e59fa7a5befca00be
GPfu <instantreplays@comcast.net>
2016-06-27 16:44:27 -0700

    values

--- hello.c
+++ hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 int main() {
-	int x = 0;
-	short y = 52;
-	printf("Var \t Val \n x \t %d \n y \t %d \n", x, y);
+	int value = 33;
+	float floatval = (float) value;
+	float pi = 3.1415926;
+	int intpi = (int) pi;
+	short strshort;
+	char tinystr[] = "X";
+	short StrShort;
+	printf("Pi as int = %d.\n", intpi );
+	printf("Str as short = %d.\n", strshort);
+	printf("Int as float = %d.\n", floatval);
 	return 0;
 }
 
