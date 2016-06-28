3d2d61d00c9eb2b679d7734f06cc890b265a0ec7
GPfu <instantreplays@comcast.net>
2016-06-28 14:28:59 -0700

    true/false (improved display)

--- hello.c
+++ hello.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 int main() {
-	printf("3 > 2 ");
-	if (3 > 2) {
+	printf("3 > 2 \t \t");
+	if (3 > 2 ) {
 		printf("true \n");
 	} else {
 		printf("false \n");
 	}
-	printf("3 < 2 ");
+	printf("3 < 2 \t \t");
 	if (3 < 2) {
                 printf("true \n");
         } else {
 	        printf("false \n");
         }
-	printf("3 - (2 + 1) ");
+	printf("3 - (2 + 1) \t");
 	if (3 - (2 + 1)) {
                 printf("true \n");
         } else {
 	        printf("false \n");
         }
-	printf("d ");
+	printf("d \t \t");
 	if ('d') {
                 printf("true \n");
         } else {
 	        printf("false \n");
         }
-	printf("9mod3 ");
+	printf("9mod3 \t \t");
 	if (9 % 3) {
                 printf("true \n");
         } else {
 	        printf("false \n");
         }
-	printf("(3 > 2) - 1 ");
+	printf("(3 > 2) - 1 \t");
 	if ((3 > 2) - 1) {
                 printf("true \n");
         } else {
 	        printf("false \n");
         }
-	printf("3 > 2 ");
+	printf("3 > 2 \t \t");
 	if (3 >	2) {
                 printf("true \n");
         } else {
@@ -45,4 +45,3 @@ int main() {
 
 	return 0;
 }
-
