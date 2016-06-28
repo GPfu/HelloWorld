9ad98fca529661ded5557b6435184235e0fdcfcb
GPfu <instantreplays@comcast.net>
2016-06-28 14:47:53 -0700

    for loop

--- hello.c
+++ hello.c
@@ -1,41 +1,9 @@
 #include <stdio.h>
 int main() {
-	printf("3 > 2 \t \t");
-	if (3 > 2 ) {
-		printf("true \n");
-	} else {
-		printf("false \n");
+	int v;
+	for (v = 1; v < 5; v++) {
+		printf("%d \t", v);
 	}
-	printf("3 < 2 \t \t");
-	if (3 < 2) {
-                printf("true \n");
-        } else {
-	        printf("false \n");
-        }
-	printf("3 - (2 + 1) \t");
-	if (3 - (2 + 1)) {
-                printf("true \n");
-        } else {
-	        printf("false \n");
-        }
-	printf("d \t \t");
-	if ('d') {
-                printf("true \n");
-        } else {
-	        printf("false \n");
-        }
-	printf("9mod3 \t \t");
-	if (9 % 3) {
-                printf("true \n");
-        } else {
-	        printf("false \n");
-        }
-	printf("(3 > 2) - 1 \t");
-	if ((3 > 2) - 1) {
-                printf("true \n");
-        } else {
-	        printf("false \n");
-        }
-
+	printf("\n");
 	return 0;
 }
